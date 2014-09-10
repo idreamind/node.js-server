15fe52e416665a298d40573c3080c5ae4222ca1f
dreamind <kos.voit@gmail.com>
2014-09-10 21:59:27 +0300

    With template

--- modules/requestHandlers.js
+++ modules/requestHandlers.js
@@ -6,11 +6,29 @@ var exec = require("child_process").exec;
 function start( response ) {
     console.log("REQUEST handler START was called");
 
-    exec("dir", function (error, stdout, stderr ) {
-        response.writeHead(200, { "Content-type": "text/plain" });
-        response.write( stdout );
-        response.end();
-    });
+    var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8">' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<p>' +
+        '<input type="submit" value="Submit text">' +
+        '</p>' + 
+        '</form>' +
+        '</body>' +
+        '</html>';
+
+    response.writeHead(200, { "Content-type": "text/html" });
+    response.write( body );
+    response.end();
+
+//    exec("dir", function (error, stdout, stderr ) {
+//        response.writeHead(200, { "Content-type": "text/plain" });
+//        response.write( stdout );
+//        response.end();
+//    });
 }
 
 function upload( response ) {
